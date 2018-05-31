d73efb16b31e7ee2f6322f61732640dd8ed29ff2
GekiRinZ <39583603+GekiRinZ@users.noreply.github.com>
2018-05-31 15:08:22 +0700

    Update Bai7.c

--- Source/Bai7.c
+++ Source/Bai7.c
@@ -2,12 +2,21 @@
 
 int main(){
     int a,b,uc,bc;
-    printf("Nhap (a,b): ");
-    scanf("%d%d",&a,&b);
+    do{
+    printf("Nhap a: ");
+    scanf("%d",&a);
+    printf("Nhap b: ");
+    scanf("%d",&b);
+    if(a==0&&b==0){
+        printf("Nhap lai gia tri a va b khac 0.\n");
+    }
+    }while(a==0&b==0);
     for (uc=a;uc>=1;uc--){
         if (a%uc==0 && b%uc==0){
             printf("UCLN(%d,%d)=%d\n",a,b,uc);
             break;
+        }else if (a==0||b==0){
+            printf("");
         }
     }
     for (bc=a;bc<=a*b;bc++){
