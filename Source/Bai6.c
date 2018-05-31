96a71e88ba56574602ae34007f4e596279ee2e0f
GekiRinZ <39583603+GekiRinZ@users.noreply.github.com>
2018-05-31 15:07:47 +0700

    Update Bai6.c

--- Source/Bai6.c
+++ Source/Bai6.c
@@ -4,22 +4,38 @@
 
 void main(){
     int i,j;
-    char s[255];
-    printf("nhap s: ");
-    gets(s);
-    fflush(stdin);
+    int smax;
+    printf("\nNhap vao so phan tu toi da cua chuoi: ");
+    scanf("%d",&smax);
+    char s[smax];
+    do{
+        printf("\nNhap vao chuoi: ");
+        fflush(stdin);
+        gets(s);
+        if(strlen(s)>smax){
+            printf("\nNhap lai chuoi thoa man dieu kien!");
+        }
+    }while(strlen(s)>smax);
+    //xoa dau cach o dau chuoi
     while (s[0]==' '){
         strcpy(&s[0],&s[1]);
     }
+    //xoa dau cach o cuoi chuoi
     while (s[strlen(s)-1]==' '){
         strcpy(&s[strlen(s)-1],&s[strlen(s)]);
     }
-    for(i=0; i<strlen(s) ;i++){
+    //chuyen chu cai dau tien thanh chu hoa
+	if('a'<=s[0]&&s[0]<='z'){
+		s[0]=s[0]-32;
+	}
+    //xoa cac dau cach canh nhau
+    for(i=0; i<strlen(s); i++){
         if ((s[i]==' ')&&(s[i+1]==' ')){
             strcpy(&s[i],&s[i+1]);
             i--;
         }
     }
+    //xoa 2 dau cham phay lien nhau
     for(i=0;i<strlen(s)-1;i++){
 		if(s[i]==','||s[i]=='.'){
 			if(s[i+1]==','||s[i+1]=='.'){
@@ -28,15 +44,16 @@ void main(){
 			}
 		}
 	}
+	//xoa dau cach truoc dau cham hoac dau phay
     for(i=0;i<strlen(s)-1;i++){
 		if(s[i]==' '&&(s[i+1]==','||s[i+1]=='.')){
 			strcpy(&s[i],&s[i+1]);
 		}
 	}
+	//them dau cach sau dau cham hoac dau phay
     for(i=0;i<strlen(s)-1;i++){
-		if(s[i]==','||s[i]=='.'&&s[i+1]!=' '){
+		if((s[i]==','||s[i]=='.')&&s[i+1]!=' '){
 			for(j=strlen(s)+1;j>=i+2;j--){
-
 				s[j]=s[j-1];
 			}
 			s[i+1]=' ';
