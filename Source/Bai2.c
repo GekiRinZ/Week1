9b04f58d3d14f35b0dbb8da8351e3fe201b21bb2
GekiRinZ <39583603+GekiRinZ@users.noreply.github.com>
2018-05-31 15:07:07 +0700

    Update Bai2.c

--- Source/Bai2.c
+++ Source/Bai2.c
@@ -2,12 +2,22 @@
 
 int main()
 {
-    char str[100];
+    int s;
     int chuthuong,chuhoa;
     int n;
     chuthuong=chuhoa=0;
-    printf("Nhap vao chuoi: ");
-    gets(str);
+    printf("\nNhap vao so phan tu toi da cua chuoi: ");
+    scanf("%d",&s);
+    char str[s];
+    do{
+        printf("\nNhap vao chuoi: ");
+        fflush(stdin);
+        gets(str);
+        if(strlen(str)>s){
+            printf("\nNhap lai chuoi thoa man dieu kien!");
+        }
+    }while(strlen(str)>s);
+
     for(n=0;str[n]!=NULL;n++){
 
         if(str[n]>='A' && str[n]<='Z')
